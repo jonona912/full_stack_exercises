0818ee3a3e04072d474b34c031e4f77cfb5b426a
doose <doose@vbox.myguest.virtualbox.org>
2026-01-05 20:57:45 +0100

    part 4a done

---
+++ part4/blog/backend/utils/list_helper.js
@@ -0,0 +1,50 @@
+const Lodash = require('lodash')
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  // how thread works e.g.
+  // Math.max(...[5, 10, 3])  // Same as Math.max(5, 10, 3) → Returns 10
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+  return blogs.find(blog => blog.likes === maxLikes)
+}
+
+const mostBlogs = (blogs) => {
+  // count how many blogs an author has
+  // accum = 0, then increment each time 
+  const grouped = Lodash.groupBy(blogs, 'author')
+  const result = Object.entries(grouped).map(([author, blogs]) => ({
+    author,
+    blogs: blogs.length
+  }))
+  const maxBlogs = Math.max(...result.map(author => author.blogs))
+  const authorWithMostBlogs = result.find(author => author.blogs === maxBlogs)
+  // console.log('=============>grouped: ', authorWithMostBlogs)
+  return authorWithMostBlogs
+}
+
+const mostLikes = (blogs) => {
+  const grouped = Lodash.groupBy(blogs, 'author')
+  const result = Object.entries(grouped).map(([author, blogs]) => ({
+    author,
+    likes: totalLikes(blogs)
+  }))
+  console.log('Result ================>', result)
+  const maxLikes = Math.max(...result.map(author => author.likes))
+  const authorWithMostLikes = result.find(author => author.likes === maxLikes)
+  return authorWithMostLikes
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
