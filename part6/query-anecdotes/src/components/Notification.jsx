84971dd2b71acd775cca99e25fd840ff89074efd
jonona912 <zulfiqor.khojazoda@gmail.com>
2026-02-28 05:30:56 +0100

    part 6 done

---
+++ part6/query-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react'
+import NotifContext from '../NotifContext'
+
+const Notification = () => {
+  const { notif } = useContext(NotifContext)
+
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  if (notif !== '') {
+    return (
+      <div style={style}>
+        <p>{notif}</p>
+      </div>
+    )
+  }
+}
+
+export default Notification
